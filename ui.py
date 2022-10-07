ad76081aecd312076da01c083255acf51fc4c47a
andrienr <e.andriolo@gmail.com>
2022-10-07 19:49:23 +0200

    better output formatting

--- ui.py
+++ ui.py
@@ -25,13 +25,19 @@ def index():
 def submit():
     if request.method == 'POST':
         review = request.form['review']
+        if len(review.split()) == 0:
+            return render_template('index.html', message='Please enter a review')
         prediction = model.predict_text(review)
-        msg = 'The review talks about '
-        cat = [c for c in prediction.keys()]
-        sent = [s for s in prediction.values()]
-        for i in range(len(prediction)):
-            msg = msg + (cat[i] + ' in a ' + sent[i] + ' way, ')
-        msg = msg[0:-2] + '.'
+        print(prediction)
+        if prediction == {}:
+            msg = 'No significant results'
+        else:
+            msg = 'The review tells about '
+            cat = [c for c in prediction.keys()]
+            sent = [s for s in prediction.values()]
+            for i in range(len(prediction)):
+                msg = msg + (cat[i] + ' in a ' + sent[i] + ' way, ')
+            msg = msg[0:-2] + '.'
         return render_template('index.html', message=msg)
 
 
